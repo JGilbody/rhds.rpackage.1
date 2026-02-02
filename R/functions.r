2224a2ea9fe92d5fc83189f9c73d7a0d4d37b0ba
JGilbody <bu19525@bristol.ac.uk>
2026-02-02 15:29:11 +0000

    package all

---
+++ R/functions.r
@@ -0,0 +1,27 @@
+#' Extract participant ID from TCGA barcode
+#' @param id TCGA barcode
+#' @return participant ID
+#' @export
+extract.participant <- function(id) {
+  sub("TCGA-[^-]+-([^-]+)-.*", "\\1", id)
+}
+
+extract.file <- function(tar.file, extract.file, new.file, resultsdir) {
+  # get file path to extracted file
+  x.file <-
+    grep(extract.file,
+      utils::untar(tar.file, list = T),
+      value = T
+    )
+    
+  # extract the tar file
+  cat("Extracting", tar.file, "to", new.file, "\n")
+  untar(tar.file, exdir=resultsdir, extras="--no-same-owner")
+  x.file = file.path(resultsdir,x.file)
+
+  # move the data to named output
+  file.copy(x.file, new.file)
+
+  # remove untared directory
+  unlink(dirname(x.file), recursive = TRUE)
+}
